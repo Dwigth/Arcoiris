0769548603033e049ee7d90c4b055b41dd92c6ca
Dwigth Astacio <dwigth32@outlook.es>
2018-02-08 00:33:01 -0600

    Prueba de rendimiento apk
    se agregaron vistas de compradores y boton para escaneo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import "rxjs/add/operator/map";
+import { LoadingController } from 'ionic-angular';
 import { BuyerServiceProvider } from '../../providers/buyer-service/buyer-service';
 
 @Component({
@@ -8,24 +9,44 @@ import { BuyerServiceProvider } from '../../providers/buyer-service/buyer-servic
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tap;
   listado;
   compradores:any[];
-  constructor(public navCtrl: NavController, private crud:BuyerServiceProvider) {
+  constructor(
+    public navCtrl: NavController,
+    private crud:BuyerServiceProvider,
+    public loadingCtrl: LoadingController) { }
 
-    this.listarproductos();
-
+  ngOnInit(){
+      this.listarproductos();
   }
 
   listarproductos(){
     //this.slim.stop();
     this.crud.listar()
-    .map((response) => response.json())
-    .subscribe((data) => {
+    .map(
+        (response) => response.json()
+      ).subscribe(
+        (data) => {
       this.listado = data;
       this.compradores = this.listado;
       console.log(this.listado);
       //this.slim.complete();
     });
 }
+
+tapEvent(ev:any){
+  if (ev.isFinal) {
+      this.presentLoading();
+        this.listarproductos();
+  }
+}
+presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Actualizando...",
+      duration: 1000
+    });
+    loader.present();
+  }
 
 }
