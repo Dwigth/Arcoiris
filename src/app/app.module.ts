3f07d324e94561b855cbc1f96bf918dd4c7c72f4
Dwigth Astacio <dwigth32@outlook.es>
2018-02-08 22:02:34 -0600

    Inicio de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { Http } from '@angular/http';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BuyerServiceProvider } from '../providers/buyer-service/buyer-service';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BuyerServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import "rxjs/add/operator/map";
+import { BuyerServiceProvider } from '../../providers/buyer-service/buyer-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  listado;
+  compradores:any[];
+  constructor(public navCtrl: NavController, private crud:BuyerServiceProvider) {
 
-  constructor(public navCtrl: NavController) {
+    this.listarproductos();
 
   }
+
+  listarproductos(){
+    //this.slim.stop();
+    this.crud.listar()
+    .map((response) => response.json())
+    .subscribe((data) => {
+      this.listado = data;
+      this.compradores = this.listado;
+      console.log(this.listado);
+      //this.slim.complete();
+    });
+}
 
 }
